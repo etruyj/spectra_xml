9a6f594a913f84d0e61f3a995d01ccdae2a92037
etruyj <etruyj@gmail.com>
2022-03-25 16:37:31 -0400

    Forgot to add this to the repo apparently. Late addition.

---
+++ code/commands/sub/GroupTapes.java
@@ -0,0 +1,25 @@
+//===================================================================
+// 	GroupTapes.java
+// 		Description:
+// 			This is an alternative method for gathering
+// 			tapes for the eject empty TeraPack method.
+// 			This will generate multiple move queue files
+// 			to be loaded in the library sequentially.
+//===================================================================
+
+package com.socialvagrancy.spectraxml.commands.sub;
+
+import com.socialvagrancy.spectraxml.commands.BasicCommands;
+import com.socialvagrancy.spectraxml.structures.Move;
+import com.socialvagrancy.utils.Logger;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GroupTapes
+{
+	public static ArrayList<Move> fromList(BasicCommands library, String partition, String file_path, Logger logbook)
+	{
+
+	}
+}
